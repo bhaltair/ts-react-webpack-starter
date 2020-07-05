002159d20e1da10ca0143e20f414fc8ff4ab6014
wangqi <njcit_wangqi@163.com>
2020-07-06 00:06:32 +0800

    feat: 一个 ts 起步项目

---
+++ README.md
@@ -0,0 +1,12 @@
+## 从零开始搭建一个 React + TypeScript 项目
+
+- 使用 `ts-loader` 编译 `ts` 代码
+- React Hook
+- splitChunks
+- webpack-dev-server
+- html-webpack-plugin
+
+## 参考
+
+[TypeScript React Conversion Guide
+](https://github.com/Microsoft/TypeScript-React-Conversion-Guide#typescript-react-conversion-guide)
---
+++ src/app.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { render } from "react-dom";
+import Hello from "./components/Hello";
+
+class App extends React.Component<{}, {}> {
+    render() {
+        return (
+            <div className="app">
+                <Hello name={'世界'}/>
+            </div>
+        )
+    }
+}
+
+render(
+    <App />, document.getElementById("content")
+);
